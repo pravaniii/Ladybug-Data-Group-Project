d5a491af8f648f81fc08ff18869cd94909c1ec54
pravaniii <pravanipradhan21@augustana.edu>
2022-12-12 15:32:19 -0600

    updated file

--- R of filtered table.R
+++ R of filtered table.R
@@ -8,4 +8,79 @@ rm(list = ls())
 
 setwd("~/Desktop/Project-Insect-Carnivore-main/Ladybug-Data-Group-Project/data")
 
-table1 <- read_excel("Ladybug Data.xlsx", .name_repair = "universal")
+table1 <- read_excel("Ladybug Data.xlsx", .name_repair = "universal") 
+
+table1$plot <- str_sub(table1$plot, 4,5) #extracting the middle two letter 
+
+nameChange <- table1 %>% #changing the names of every plot to it's full form 
+  dplyr::mutate(plot = ifelse(plot == "AG", "Agriculture", plot)) %>%
+  dplyr::mutate(plot = ifelse(plot == "GF", "Forested", plot)) %>%
+  dplyr::mutate(plot = ifelse(plot == "GM", "Mowed Grass", plot)) %>%
+  dplyr::mutate(plot = ifelse(plot == "GU", "Unmowed Grass", plot)) %>%
+  dplyr::mutate(plot = ifelse(plot == "PR", "Praire", plot)) %>%
+  dplyr::mutate(plot = ifelse(plot == "GA", "Garden", plot)) %>%
+  dplyr::mutate(plot = ifelse(plot == "IC", "Industrial", plot)) %>%
+  
+  dplyr::mutate(identifier = ifelse(identifier == "Jack Hughes", "J. Hughes", identifier)) %>%
+  dplyr::mutate(identifier = ifelse(identifier == "jack hughes", "J. Hughes", identifier)) %>%
+  dplyr::mutate(identifier = ifelse(identifier == "Veronica Cervantes", "V. Cervantes", identifier)) %>%
+  dplyr::mutate(identifier = ifelse(identifier == "v. cervantes", "V. Cervantes", identifier)) %>%
+  dplyr::mutate(identifier = ifelse(identifier == "Olivia Ruffatto", "O. Ruffatto", identifier)) %>%
+  dplyr::mutate(identifier = ifelse(identifier == "Marissa Gorsegner", "M. Gorsegner", identifier)) %>%
+  dplyr::mutate(identifier = toupper(identifier)) %>%
+  dplyr::mutate(identifier = ifelse(identifier == "O RUFFATTO", "O. RUFFATTO", identifier)) %>%
+  
+  dplyr::mutate(collector = toupper(collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "JACK HUGHES", "J. HUGHES", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "J HUGHES", "J. HUGHES", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "J HUGEES", "J. HUGHES", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "J. HUGHEES", "J. HUGHES", collector)) %>%
+  
+  dplyr::mutate(collector = ifelse(collector == "OLIVIA RUFFATTO", "O. RUFFATTO", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "O. RUFFATTTO", "O. RUFFATTO", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "OLIVIARUFFATTO", "O. RUFFATTO", collector)) %>%
+  
+  dplyr::mutate(collector = ifelse(collector == "VERONICA CERVANTES", "V. CERVANTES", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "V.CERVANTES", "V. CERVANTES", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "V CERVANTES", "V. CERVANTES", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "VERONICA CERVATNES", "V.CERVANTES", collector)) %>%
+  
+  dplyr::mutate(collector = ifelse(collector == "MARISSA GORSEGNER", "M. GORSEGNER", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "M.GORSEGNER", "M. GORSEGNER", collector)) %>%
+  dplyr::mutate(collector = ifelse(collector == "M GORSEGNER", "M. GORSEGNER", collector)) 
+ 
+SpeciesByPlot <- nameChange %>%
+  dplyr::count(plot) #counting the sum of every species by plot
+
+ggplot(data = SpeciesByPlot, aes(x = plot, y= n)) + 
+  geom_bar(stat = "identity", fill = "steel blue") + 
+  geom_text(aes(label =n), vjust = -0.3, size = 3.5) +
+  theme_minimal()
+
+speciesPlot <- nameChange %>%
+  dplyr::group_by(plot, Species) %>%
+  dplyr::summarise(count =n())
+
+specificSpeciesCount <- nameChange %>%
+  dplyr::select(Species, plot) %>%
+  dplyr::filter(Species == "Coleomegilla maculata") %>%
+  dplyr::group_by(Species, plot) %>%
+  dplyr::summarise(count = n())
+
+comparePlot <- nameChange %>%
+  dplyr::select(Species, plot) %>%
+  dplyr::filter(plot == "Mowed Grass") %>%
+  dplyr::group_by(plot, Species) %>%
+  dplyr::summarise(count = n())
+
+Identifier <- nameChange %>%
+  dplyr::count(identifier) 
+
+
+#GM = mowed grass
+#GU = unmowed grass
+#AG = agriculture
+#IC = I think this is industrial but im not 100%
+#PR = prairie
+#GA = garden
+#GF = forested
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Project-Insect-Carnivore
+
+
+1. Among the seven different plots, which plot had the most number of species recorded? 
+
+Agriculture = 88
+Forested = 36
+Garden = 58
+Industrial = 112
+Mowed Grass = 39
+Praire = 88
+Unmowed Grass = 137
+
+    - What was the difference in the number of species found in similar environments
+    We made a bar graph plotting the species found in Unmowed grass and mowed grass as we wanted to see difference in the varaieties of species found. 
+
+
+2. Who identified the most number of species? Were they also the ones who collected the species? 
+
+From the data calculated, we see that the number of times a person has collected and identified species are not the same. For instance, J.Hughes has collected 136 species however, he has identified 203. Therefore he has identified 67 more than he has collected. Whereas M.Gorsengner has collected 137 species by only identified 55. 
+
+3. How many different species we found in each decade? 
+1950s = 5
+1960s = 10
+1970s = 15
+1980s = 12
+1990s = 6
+2000s = 4
+2010s = 3
+2020s = 14
+
+4. How many species were found in the different states around the US?
+Montana = 1
+Minnesota = 6
+Michigan = 1
+Iowa = 331
+Illinois = 296
+Colorado = 2
+
+5. What was the result of the t-test conducted? 
+we conducted a t-test on the average species found in every plot. The result of the t-test were as follows:
+
+data:  averageData$average_plot
+t = 5.6029, df = 6, p-value = 0.001377
+alternative hypothesis: true mean is not equal to 0
+95 percent confidence interval:
+ 22.45072 57.26357
+sample estimates:
+mean of x 
+ 39.85714
+ 
+From the results above we see that the p=value is 0.001377 which indicates that there is string evidence against the null hypothesis. 
+
+
+
+
+
+
+
+
+
